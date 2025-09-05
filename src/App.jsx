c06d4df95be5923b90f8cc167cabcf5600246407
Tavan Mohammed <tavan.mohammed19@gmail.com>
2025-09-05 09:56:08 -0400

    fix: correct case in import paths for Linux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 import Gallery from "./pages/GalleryPage"
 import Album from "./pages/Album";
 import FAQPage from "./pages/Faq";
